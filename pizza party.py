af8c38239a743f31e12aa69199850b6c2a90e9f6
jahnavinettim <118586116+jahnavinettim@users.noreply.github.com>
2024-06-15 21:34:39 +0530

    Add files via upload

---
+++ pizza party.py
@@ -0,0 +1,14 @@
+a,y=map(int,input().split())
+ans=0
+while True:
+    if a%y==0:
+        ans=y
+        break
+    else:
+        y=y+1
+s=0
+while ans>0:
+    digit=ans%10
+    s=s+digit
+    ans=ans//10
+print(s)
